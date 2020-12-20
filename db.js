495f301120b779f0343903f06bb2ba9404d6f974
KAT Binene <asaphbinene@outlook.com>
2020-12-20 05:04:35 +0200

    my test of deployment 4

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ if(port == null || port == ""){
   port = 7329;
 }
 app.use(express.static('public'));
-let connectionString = 'mongodb+srv://katbinUser01:myKatbinUser01@Mango@cluster0.cbcb8.mongodb.net/katbinTodoApp?retryWrites=true&w=majority';
+let connectionString = 'mongodb+srv://katbinUser01:myKatbinUser01@Mango@cluster0.cbcb8.mongodb.net/mykatbindb?retryWrites=true&w=majority';
 mongodb.connect(connectionString, {useUnifiedTopology: true}, function(err, client){
     db = client.db();
     app.listen(port);
